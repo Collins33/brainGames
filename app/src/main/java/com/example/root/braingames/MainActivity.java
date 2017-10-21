216f0aa5695712548b3a052fd0dacbd60ebc4cc5
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-10-21 12:13:49 +0300

    I have added the intent to go to the activity with the game

--- app/src/main/java/com/example/root/braingames/MainActivity.java
+++ app/src/main/java/com/example/root/braingames/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.root.braingames;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startGame=(Button) findViewById(R.id.StartGamebutton);
+        startGame.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view){
+        Intent intent=new Intent(getApplicationContext(),secondActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
