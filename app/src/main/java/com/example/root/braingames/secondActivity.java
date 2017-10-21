863aa0c96fcb86a74d2df5117a6cf8407067786c
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-10-21 13:34:24 +0300

    I have added the layout to show the instructions and added the button that makes the layout invisible once clicked by changing alpha to 0

--- app/src/main/java/com/example/root/braingames/secondActivity.java
+++ app/src/main/java/com/example/root/braingames/secondActivity.java
@@ -2,12 +2,24 @@ package com.example.root.braingames;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
-public class secondActivity extends AppCompatActivity {
-
+public class secondActivity extends AppCompatActivity implements View.OnClickListener{
+    Button startGame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+         startGame=(Button) findViewById(R.id.startGame);
+         startGame.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view){
+        if (view==startGame){
+            RelativeLayout relativeLayout=(RelativeLayout) findViewById(R.id.relativeLayoutInstructions);
+            relativeLayout.setAlpha(0);
+        }
     }
 }
