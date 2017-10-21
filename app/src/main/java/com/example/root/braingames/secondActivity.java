278f66e03f409436072f8c3d45041eb8d4ae10af
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-10-21 14:50:52 +0300

    I have added an array of answers to deal with adding the right answer and wrong answers to the view.The correct answer is added to the right position

--- app/src/main/java/com/example/root/braingames/secondActivity.java
+++ app/src/main/java/com/example/root/braingames/secondActivity.java
@@ -7,11 +7,16 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class secondActivity extends AppCompatActivity implements View.OnClickListener{
     Button startGame;
     TextView triviaSum;
+    //location of correct answer in the layout
+    int locationOfCorrectAnswer;
+    //will contain answers
+    ArrayList<Integer> answers=new ArrayList<Integer>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +24,43 @@ public class secondActivity extends AppCompatActivity implements View.OnClickLis
          startGame=(Button) findViewById(R.id.startGame);
          triviaSum=(TextView) findViewById(R.id.textTrivia);
          startGame.setOnClickListener(this);
+        //buttons to display the answers
+        Button button0=(Button) findViewById(R.id.button0);
+        Button button1=(Button) findViewById(R.id.button1);
+        Button button2=(Button) findViewById(R.id.button2);
+        Button button3=(Button) findViewById(R.id.button3);
+
         //generate random numbers for the trivia
         Random rand=new Random();
-        int a=rand.nextInt(1001);
-        int b=rand.nextInt(1001);
+        int a=rand.nextInt(21);
+        int b=rand.nextInt(21);
         //set the textview
         triviaSum.setText(Integer.toString(a) + " + " +Integer.toString(b));
+        //location is based on the tag of buttons which is from 0-3
+        locationOfCorrectAnswer=rand.nextInt(4);
+        //setting correct answer
+        for(int i=0;i<4;i++){
+            //if the button is the loacation of the correct answer,
+            //add the correct to the arrayList
+            int incorrectAnswer;
+            if(i==locationOfCorrectAnswer){
+                answers.add(a+b);
+            }
+            //if it is not the location of the correct answer
+            else{
+                 incorrectAnswer=rand.nextInt(41);
+                while(incorrectAnswer== a+b){
+                    //stop generation of correct answer in wrong answer layout
+                    incorrectAnswer=rand.nextInt(41);
+                }
+                answers.add(incorrectAnswer);
+            }
+        }
+        button0.setText(Integer.toString(answers.get(0)));
+        button1.setText(Integer.toString(answers.get(1)));
+        button2.setText(Integer.toString(answers.get(2)));
+        button3.setText(Integer.toString(answers.get(3)));
+
     }
     @Override
     public void onClick(View view){
