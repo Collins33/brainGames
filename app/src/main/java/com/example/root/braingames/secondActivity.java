908b7bce0e3bd990803843d062716e228e256c01
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-10-21 14:11:24 +0300

    I have added a random number generator to generate random numbers from 1 to 1000 and display the number on the textview

--- app/src/main/java/com/example/root/braingames/secondActivity.java
+++ app/src/main/java/com/example/root/braingames/secondActivity.java
@@ -5,15 +5,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class secondActivity extends AppCompatActivity implements View.OnClickListener{
     Button startGame;
+    TextView triviaSum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
          startGame=(Button) findViewById(R.id.startGame);
+         triviaSum=(TextView) findViewById(R.id.textTrivia);
          startGame.setOnClickListener(this);
+        //generate random numbers for the trivia
+        Random rand=new Random();
+        int a=rand.nextInt(1001);
+        int b=rand.nextInt(1001);
+        //set the textview
+        triviaSum.setText(Integer.toString(a) + " + " +Integer.toString(b));
     }
     @Override
     public void onClick(View view){
