6b8293c49589681514b9af266c7196cbb8f3f1ab
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-10-22 22:25:08 +0300

    I have edited the layout for the main activity

--- app/src/main/java/com/example/root/braingames/MainActivity.java
+++ app/src/main/java/com/example/root/braingames/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.root.braingames;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -13,6 +15,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button startGame=(Button) findViewById(R.id.StartGamebutton);
+        TextView mainText=(TextView) findViewById(R.id.mainText);
+        Typeface amatic=Typeface.createFromAsset(getAssets(),"fonts/Amatic-Bold.ttf");
+        mainText.setTypeface(amatic);
         startGame.setOnClickListener(this);
     }
     @Override
